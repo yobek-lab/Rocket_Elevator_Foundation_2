aec7019aacb71ab949c0da768382d2c52c6fc4e2
yobek-lab <54604022+yobek-lab@users.noreply.github.com>
2020-04-09 10:42:38 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.12.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "younesbekkali"
-set :repo_url, "https://github.com/jorgechavarriaga/Rocket-Elevator-Foundation.git"
+set :repo_url, "https://github.com/yobek-lab/Rocket_Elevator_Foundation_2.git"
 # Changed for the Foundation with my git and app
 # set :application, "Ukeme"
 # set :repo_url, "https://github.com/kembasy/Rocket_Elevators_API"
