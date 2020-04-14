1444da38c7a1009eb80b5dc4a4e25a67837c494a
yobek-lab <Y.bekkali@GMAIL.com>
2020-04-14 15:45:40 -0400

    lockable for X minutes after Y attempts,, two columns added to the database

--- db/schema.rb
+++ db/schema.rb
@@ -127,6 +127,9 @@ ActiveRecord::Schema.define(version: 2020_03_12_171141) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "locked_at"
+    t.integer "failed_attempts", default: 0, null: false 
+  
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
