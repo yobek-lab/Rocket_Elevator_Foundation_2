45dad7c5869920478501be9d57d763525e65febd
yobek-lab <Y.bekkali@GMAIL.com>
2020-04-14 14:17:22 -0400

    Create geolocation.rb,, timeoutable

---
+++ app/models/geolocation.rb
@@ -0,0 +1,21 @@
+class Intervention < ApplicationRecord
+
+    # devise :database_authenticatable, 
+    #        :recoverable, :rememberable, :validatable
+           # to be deleted
+      devise :timeoutable
+    #   belongs_to :employee, optional: true
+    #   belongs_to :customer, optional: true
+    #   belongs_to :building, optional: true
+    #   belongs_to :battery, optional: true
+    #   belongs_to :column, optional: true
+    #   belongs_to :elevator, optional: true
+    #   belongs_to :employee, optional: true
+     
+      
+      # enum intervention_result: [:Success, :Failed, :Incomplete]
+    
+      # enum intervention_status: [:Pending, :InProgress, :Interrupted, :Resumed, :Completed]
+    
+     
+    end
