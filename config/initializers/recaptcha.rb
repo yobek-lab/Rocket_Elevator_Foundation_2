543ebc605a5c4ba75e08f0742624d6028e1dc1d4
yobek-lab <Y.bekkali@GMAIL.com>
2020-04-16 19:54:35 -0400

    captcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,10 +1,10 @@
 Recaptcha.configure do |config|
     config.site_key  = ENV['recaptcha_site_key']
     config.secret_key = ENV['recaptcha_secret_key']
-    #V3
-    # config.site_key  = "6LcCJ-oUAAAAAB7G0btEWxKEOGGlrmT71YhtbtA1"
-    # config.secret_key = "6LcCJ-oUAAAAAM10L2z2NShNiXixMNapFdHCer66"
-    #V2
+    # #V3
+    # # config.site_key  = "6LcCJ-oUAAAAAB7G0btEWxKEOGGlrmT71YhtbtA1"
+    # # config.secret_key = "6LcCJ-oUAAAAAM10L2z2NShNiXixMNapFdHCer66"
+    # #V2
     # config.site_key  = "6Le5J-oUAAAAAIG8kedam9qYhQ9p3ysOG8JRvg1C"
     # config.secret_key = "6Le5J-oUAAAAAETwJgEtMWJ_P9hqMweuHO4MHU_Z"
 end
